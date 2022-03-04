3650311504b41584da4609a4a4519423932cb14d
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-03-04 17:57:40 +0100

    added product properties

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public class BlazorModel
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
+        public IEnumerable<ProductProp> ProductProperties { get; set; }
+
     }
 }
