41bb41b23e07d0da4c0b1a1b5db076fd28d3de5e
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-03-09 15:33:55 +0100

    added toastr and sweet alerts using js

---
+++ BlazorWeb_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorWeb_Server.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async Task<ValueTask> ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+            return ValueTask.CompletedTask;
+        }
+
+        public static async Task<ValueTask> ToastrFailed(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+            return ValueTask.CompletedTask;
+
+        }
+    }
+}
