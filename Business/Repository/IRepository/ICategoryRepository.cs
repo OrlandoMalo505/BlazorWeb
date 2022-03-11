01c52ab1620b9ae8ecaf91a20fb324846d685db9
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-03-11 12:43:52 +0100

    added create method and automapper

---
+++ Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public CategoryDTO Create(CategoryDTO objDTO);
+        public CategoryDTO Update(CategoryDTO objDTO);  
+        public IEnumerable<CategoryDTO> GetAll();
+        public CategoryDTO Get(int id);
+        public int Delete(int id);
+
+    }
+}
